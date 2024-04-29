896cc3a474f31d98af4d0edb9c22b38dceea3581
jkhemcha <jkhemcha@ualberta.ca>
2024-04-29 17:21:10 -0600

    Modified from page and resume.

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -11,29 +11,14 @@ const About = () => {
       <h2>About Me</h2>
       <div className='about-wrapper'>
         <div className='info' style={style}>
-            <h5>Education</h5>
-            4th year Computing Science student at the University of Alberta.
-            <br></br><br></br>
-            <h5>Career goals</h5>
-            My short-term objective is to channel my 
-            passion for technology into a career where I have the opportunity to work with other like-minded
-            individuals and improve the lives of those around me.
+            <h5>Who am I?</h5>
+            I am a Software Developer based out of Edmonton, Alberta, Canada with a degree in Computing Science from the University of Alberta.
             <br></br><br></br>
             <h5>Experience</h5>
-            <p>For more detailed information about my relevent skills and experience,</p>
+            <p>For detailed information about my relevent skills and experience,</p>
             <button className="btn resumebutton" style={style2}><a href={resume} target="_blank" rel="noreferrer">Download my resume</a></button>
             <br></br><br></br>
             <hr />
-            <h5>Personal life</h5>
-            In my spare time I love to attend
-            festivals and live music events around the world,
-            especially electronic music events.
-            <br></br><br></br>
-            My newest obsession is DJing, and
-            so far I have played at multiple events, giving me the chance
-            to connect with and meet other likeminded people and meet
-            some of my musical heroes. 
-            <br></br><br></br>
         </div>
         <img className='me' alt="me!" src={jensen}></img>
       </div>
